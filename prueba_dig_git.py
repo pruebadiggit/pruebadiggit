c78ba7ba56abfe269ba591930e790272b7cd995f
pruebadiggit <100242068+pruebadiggit@users.noreply.github.com>
2022-02-22 17:14:01 -0500

    Creación nuevo archivo python lectura de csv

---
+++ prueba_dig_git.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[10]:
+
+
+df=pd.read_excel('C:/Users/2076222/OneDrive - TCS COM PROD/Documents/prueba_dig.xlsx')
+
+
+# In[11]:
+
+
+df['MILITAR']=np.where(df['CARGA']>1000,'Si','No')
+
+
+# In[12]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
